2baab4bacbd17d470fd7909b87ba16f7243627ea
hongzhenquan <hongzhenquan@xianqingzao.com>
2019-09-29 18:12:42 +0800

    递归所有，删除注释行

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
 const loaderUtils = require('loader-utils');
 
-let includeNoteExp = new RegExp("\\/\\*include(.*)start\\*\\/[\\s\\S]*\\/\\*include end\\*\\/");
-let excludeNodeExp = new RegExp("\\/\\*exclude(.*)start\\*\\/[\\s\\S]*\\/\\*exclude end\\*\\/");
-let moduleExp = new RegExp("require\\(\"\\.\\/.*\\?(.*)\"\\)");
+let codeIncludeExp = new RegExp("\\/\\*include(.*?)start\\*\\/([\\s\\S]*?)\\/\\*include end\\*\\/");  // 非贪婪模式
+let codeExcludeExp = new RegExp("\\/\\*exclude(.*?)start\\*\\/([\\s\\S]*?)\\/\\*exclude end\\*\\/");
+let moduleExp = new RegExp("require\\(\"\\.\\/.*?\\?(.*?)\"\\)");
+let xmlIncludeExp = new RegExp("<!--include(.*?)start-->([\\s\\S]*?)<!--include end-->");
+let xmlExcludeExp = new RegExp("<!--exclude(.*?)start-->([\\s\\S]*?)<!--exclude end-->");
 
-module.exports = function (source, option) {
-    const options = loaderUtils.getOptions(this);
-    let profile = options.profile || "";
-
-    let matchs = source.match(includeNoteExp);
-    if (matchs && !matchs[1].includes(' ' + profile + ' ')) {  // include注释 里不包含当前profile，则将其删除
-        source = source.replace(matchs[0], '');
+// 处理 /*include profile*/ <!--include profile-->
+function removeNotInclude(source, profile) {
+    let matchs = source.match(codeIncludeExp) || source.match(xmlIncludeExp);
+    if (matchs) {
+        if (!matchs[1].includes(' ' + profile + ' ')) { // include注释 里不包含当前profile，则将其删除
+            source = source.replace(matchs[0], '');
+        } else {
+            source = source.replace(matchs[0], matchs[2]);  // 否则只去掉注释
+        }
+        // 一个文件里可能有多个注释，递归处理全部
+        source = removeNotInclude(source, profile);
     }
+    return source;
+}
 
-    matchs = source.match(excludeNodeExp);
-    if (matchs && matchs[1].includes(' ' + profile + ' ')) {  // exclude注释 里包含当前profile，则将其删除
-        source = source.replace(matchs[0], '');
+// 处理 /*exclude profile*/ <!--exclude profile-->
+function removeIsExclude(source, profile) {
+    let matchs = source.match(codeExcludeExp) || source.match(xmlExcludeExp);
+    if (matchs) {
+        if (matchs[1].includes(' ' + profile + ' ')) { // include注释 里不包含当前profile，则将其删除
+            source = source.replace(matchs[0], '');
+        } else {
+            source = source.replace(matchs[0], matchs[2]);  // 否则只去掉注释
+        }
+        // 一个文件里可能有多个注释，递归处理全部
+        source = removeIsExclude(source, profile);
     }
+    return source;
+}
 
-    matchs = source.match(moduleExp);   // import './mock?include=local&exclude=prod' 会被转成 require("./mock?include=local&exclude=prod")
+// 处理 import "module?include=xx&exclude=xx"
+function removeModule(source, profile) {
+    let matchs = source.match(moduleExp);   // import './mock?include=local&exclude=prod' 会被转成 require("./mock?include=local&exclude=prod")
     if (matchs) {
         let segments = matchs[1].split('&');
         segments.forEach((item) => {
@@ -29,6 +49,21 @@ module.exports = function (source, option) {
                 source = source.replace(matchs[0], '');
             }
         });
+        // 一个文件里可能有多条，递归处理全部
+        source = removeModule(source, profile);
     }
+    return source;
+}
+
+module.exports = function (source) {
+    const options = loaderUtils.getOptions(this);
+    let profile = options.profile || "";
+
+    source = removeNotInclude(source, profile);
+
+    source = removeIsExclude(source, profile);
+
+    source = removeModule(source, profile);
+
     return source;
 };
